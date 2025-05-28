3b51bb7d868ce0269bd1f316a41ca3e047f71e11
JoelSRJE <JoelSRJE@hotmail.com>
2025-05-28 20:52:55 +0200

    Folder is a bit wonky, npms and changes should go through now

--- study-tool/src/app/components/Study Tool/StudyTool.tsx
+++ study-tool/src/app/components/Study Tool/StudyTool.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import TasksTool from "../Study Comps/Tasks/TasksTool";
+import PomodoroTool from "../Study Comps/Pomodoro/PomodoroTool";
+import FlashCardsTool from "../Study Comps/Flash Cards/FlashCardsTool";
 
 const StudyTool = () => {
-  return <div>StudyTool</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 w-full h-full p-4">
+      <h1 className="font-bold text-5xl md:text-6xl bg-gradient-to-r from-[#eecda3] to-[#ef629f] bg-clip-text text-transparent text-center">
+        Study Tools
+      </h1>
+
+      <div className="flex flex-col lg:flex-row gap-8 justify-center items-center ">
+        <section className="w-full max-w-[20rem] h-[30rem]">
+          <TasksTool />
+        </section>
+
+        <section className="w-full max-w-[20rem] h-[30rem]">
+          <PomodoroTool />
+        </section>
+
+        <section className="w-full max-w-[20rem] h-[30rem]">
+          <FlashCardsTool />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default StudyTool;
--- study-tool/src/app/page.tsx
+++ study-tool/src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import StudyTool from "./components/Study Tool/StudyTool";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center w-full min-h-full overflow-auto">
-      Placeholder
+      <StudyTool />
+      <ToastContainer />
     </main>
   );
 }
