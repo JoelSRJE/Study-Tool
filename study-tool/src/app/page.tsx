55e04018a9ddaf250b0e56771982a036c038f923
JoelSRJE <JoelSRJE@hotmail.com>
2025-05-28 19:57:44 +0200

    Setup

---
+++ study-tool/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center w-full min-h-full overflow-auto">
+      Placeholder
+    </main>
+  );
+}
