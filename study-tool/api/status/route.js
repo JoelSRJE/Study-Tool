fbac0d107dd5c65fa34252f2f5d48535f2e3862c
JoelSRJE <JoelSRJE@hotmail.com>
2025-08-26 21:07:41 +0200

    Rewrite, forgot cors

--- study-tool/api/status/route.js
+++ study-tool/api/status/route.js
@@ -6,5 +6,12 @@ export async function GET() {
     timestamp: currentTime.toISOString(),
   };
 
-  return Response.json(responseData);
+  return new Response(JSON.stringify(responseData), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
